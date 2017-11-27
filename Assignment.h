8f5bb906f4d05eac0b17e9ea35ceb1906cc258a2
michaelarobertsjr <34015906+michaelarobertsjr@users.noreply.github.com>
2017-11-27 14:17:13 -0500

    Update Assignment.h

--- Assignment.h
+++ Assignment.h
@@ -1,1 +1,24 @@
+#ifndef _ASSIGNMENT_H_
+#define _ASSIGNMENT_H_
 
+#include <string.h>
+
+using namespace std;
+
+Class Assignment {
+  
+  
+  
+}
+
+
+
+//write all methods after this
+
+
+
+
+
+
+//write all methods before this
+#endif
