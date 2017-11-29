7ee84d76a3f6038b1d38a50ca7001741bf5f1260
Dustin Durden <dustindurden@yahoo.com>
2017-11-29 17:55:38 -0500

    Update Assignment.h
    defined methods declared in last update

--- Assignment.h
+++ Assignment.h
@@ -7,16 +7,20 @@ using namespace std;
 
 Class Assignment {
   
+  vector<float > grades;
+  float high;
+  float low;
+  float mean;
   
 public: 
   
   
-    //Passes in vector containing grades to calculate high
-    float calculateHigh(vector<int>);
-    //Passes in vector containing grades to calculate low
-    float calculateLow(vector<int>);
-    //Passes in vector containing grades to calculate mean
-    float calculateMean(vector<int>);
+    //Pass in vector containing grades to calculate stats
+    float calculateHigh(vector<float > grades);
+    float calculateLow(vector<float > grades);
+    float calculateMean(vector<float > grades);
+  
+    //Accessor methods
     float getHigh();
     float getLow();
     float getMean();
@@ -29,6 +33,55 @@ public:
 
 //write all methods after this
 
+
+float Assignment::calculateHigh(vector<float > grades){
+    
+    float high = 0;
+    for (int i = 0; i < grades.size(); i++) {
+        if (grades[i] > high) {
+            high = grades[i];
+        }
+    }
+    
+    return high;
+}
+
+float Assignment::calculateLow(vector<float > grades) {
+    
+    float low = 100;
+    for (int i = 0; i < grades.size(); i++) {
+        if (grades[i] < low) {
+            low = grades[i];
+        }
+    }
+    
+    return low;
+}
+
+float Assignment::calculateMean(vector<float > grades) {
+    
+    float sum = 0;
+    for (int i = 0; i < grades.size(); i++) {
+        sum += grades[i];
+    }
+    
+    return sum / grades.size();
+}
+
+float Assignment::getHigh() {
+    
+    return this->high;
+}
+
+float Assignment::getLow() {
+    
+    return this->low;
+}
+
+float Assignment::getMean() {
+    
+    return this->mean;
+}
 
 
 
