cb98778ca9a7fb2560802540cc6efbe42be67643
Ali Sammour <34039962+itsali3@users.noreply.github.com>
2017-11-29 20:17:45 -0500

    Update Assignment.h

--- Assignment.h
+++ Assignment.h
@@ -67,7 +67,38 @@ float Assignment::calculateMean(vector<float > grades) {
     
     return sum / grades.size();
 }
+//I think .size will give us a bit value of the actual size of the array, not
+//the amt of elements, we may need to use *sizeof(grades)/sizeof(grades[0])*
+float Assignment::calculateMedian(vector<float > grades) {
+    //Odd student size case
+    if (grades.size()%2 == 1){
+      return grades[(1+grades.size())/2-1];
+    }
+    //Even student size case
+    else{
+      return (grades[grades.size()/2-1] + grades[grades.size()/2])/2;
+    }
+}
 
+//assuming grade array will be in order for mode calculation
+float Assignment::calculateMode(vector<float > grades) {
+    int mode = 1;
+    int currentMode = 0;
+    float possibleMode = grades[0];
+    for (int i = 1; i < grades.size(); ++i){
+      if grades[i] == grades[i-1]){
+        currentMode++;
+      }
+      else{
+        if (currentMode > mode){
+          possibleMode = grades[i-1];
+          mode = currentMode;
+          currentMode = 0;
+        }
+      }
+    }
+    return possibleMode;
+}
 float Assignment::getHigh() {
     
     return this->high;
