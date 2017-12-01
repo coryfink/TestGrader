940df61cf4ef3b22347beb6d0a7f0321b9ca164d
michaelarobertsjr <34015906+michaelarobertsjr@users.noreply.github.com>
2017-12-01 14:11:39 -0500

    Create Assignment.h

---
+++ Assignment.h
@@ -0,0 +1,22 @@
+#ifndef _ASSIGNMENT_H_
+#define _ASSIGNMENT_H_
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Assignment {
+  private:
+    string assignmentName;
+    vector<Test> classTests;
+    float high;
+    float low;
+    float mean;
+    float curve;
+  public:
+
+}
+
+
+#endif
