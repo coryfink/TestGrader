41f2760c5e03911efa657a8dd2d085405a00d170
mprovosty <34016170+mprovosty@users.noreply.github.com>
2017-12-02 13:14:41 -0500

    Update Test.h

--- Test.h
+++ Test.h
@@ -13,29 +13,32 @@ class Test {
 		string name;
 		string assignment_name;
 		float test_grade;
-		ifstream test_name;
+		ifstream sourceCode;
 		string filename
+		int correct_answers
 
 		vector<string> student_answers;//where we will store new test answers
 		vector<string> fill_in_the_blank;//where we fill in the fill in the blank answers
 		vector<string> key;//the correct answers to the text
 		vector<string> FitB_key; //correct answers to fill in the blank
 
+		vector<bool> correctness;//to make grading easier
 	public: 
-		Test();
+		Test(string assignmentName);
 
 		void set_name(string student_name);
 
 		void answer_input(string filename);
 
-		void grader(vector<string> student_answers);
-		void FitB_grader(vector<string> fill_in_the_blank);
+		float grader(vector<string> student_answers);
+		float FitB_grader(vector<string> fill_in_the_blank);
 
-		void omitQuestions(vector<string> student_answers);
+		void omitQuestion(vector<string> student_answers);
 };
 
-Test::Test() {
+Test::Test(string assignmentName) {
 	name = "";
+	assignment_name = assignmentName;
 	test_grade = NULL;
 }
 
@@ -44,62 +47,80 @@ void set_name(string student_name) {
 }
 
 void answer_input(string filename) {
-	cout<<"Please enter the name of text file containing the assignment: ";
-	cin>>filename;
 	sourceCode.open(filename);
 
-		//below is for failing to find a file of the input name
-		while(sourceCode.fail()) {
-			cout<<"Error: there is no file named "<<filename<<endl;
-			cout<<"Please enter the name of the input file: ";
-			cin>>filename;
-			sourceCode.open(filename);
-		}
+	//below is for failing to find a file of the input name
+	while(sourceCode.fail()) {
+		cout<<"Error: there is no file named "<<filename<<endl;
+		cout<<"Please enter the name of the input file: ";
+		cin>>filename;
+		sourceCode.open(filename);
+	}
 
-		//now filling the array representing the correct answers
-		string currentLine;
+	//now filling the array representing the correct answers
+	string currentLine;
 
-		while(getline(sourceCode,currentLine)) {
-			//checking to see if the current line is an answer and not a question of student name
-			if(currentLine.find("?")==-1 && currentLine.find("Name")==-1) {
-				student_answers.push_back(currentLine);
-			}
-		}
-}
-
-void Assignment::grader(vector<string> student_answers) {
-	for (vector<string>::iterator key_iter = key.begin() ; key_iter != key.end() ; key_iter++) {
-		for (vector<string>::iterator iter = student_answers.begin() ; iter != student_answers.end() ; iter++) {//initializes the iterator method from vector class, then starts at the beginning of the vector until it reaches the end comparing the answers of a test to the key
-			if (*iter == *key_iter) {
-				cout << "answer " << *iter << " is correct\n"; //waiting for other methods to see how we want to deal with correct/incorrect answers
-
-				this->test_grade = true;
-			} else {
-				cout << "answer " << *iter << " is incorrect\n";
-
-				this->test_grade = false;
-			}
+	while(getline(sourceCode,currentLine)) {
+		//checking to see if the current line is an answer and not a question of student name
+		if(currentLine.find("?")==-1 && currentLine.find("Name")==-1) {
+			student_answers.push_back(currentLine);
 		}
 	}
 }
 
-void Assignment::FitB_grader(vector<string> fill_in_the_blank) {
-	for (vector<string>::iterator FitB_key_iter = FitB_key.begin() ; FitB_key_iter != FitB_key.end() ; FitB_key_iter++) {
-		for (vector<string>::iterator FitB_iter = fill_in_the_blank.begin() ; FitB_iter != fill_in_the_blank.end() ; FitB_iter++) {
-			if (*FitB_iter == *FitB_key_iter) {
-				cout << "answer " << *FitB_iter << " is correct\n";
-
-				this->test_grade = true;
-			} else {
-				cout << "answer " << *FitB_iter << " is incorrect\n";
-
-				this->test_grade = false;
-			}
+float Assignment::grader(vector<string> student_answers) {
+	int key_size = key.size();//make the size a constant to reduce complexity
+	for (int i = 0; i < key_size ; i++) {
+		if (student_answers[i] == key[i]) {
+			cout << "the answer is correct" << endl;
+			//will compare the two and if they are the same, set the correctness array equal to true
+			correctness[i] = true;
+		} else {
+			cout << "the answer is incorrect" << endl;
+			//same thing but sets it to false
+			correctness[i] = false;
 		}
 	}
+	
+	correct_answers = 0;
+
+	for (vector<bool> iter = correctness.begin() ; iter < correctness.end() ; iter++) {
+		if (correctness[i]) {//if the answer is correct
+			correct_answers++;
+		}
+	}
+
+	test_grade = correct_answers / correctness.size();
+	return test_grade;
 }
 
-void Test::omitQuestion(vector<string> student_answers, int deletedIndex)
+/*float Assignment::FitB_grader(vector<string> student_answers) {
+	int key_size = FitB_key.size();
+	for (int i = 0; i < key_size ; i++) {
+		if (student_answers[i] == FitB_key[i]) {
+			cout << "the answer is correct" << endl;
+
+			correctness[i] = true;
+		} else {
+			cout << "the answer is incorrect" << endl;
+
+			correctness[i] = false;
+		}
+	}
+
+	correct_answers = 0;
+
+	for (vector<bool> iter = correctness.begin() ; iter < correctness.end() ; iter++) {
+		if (correctness[i]) {//if the answer is correct
+			correct_answers++;
+		}
+	}
+
+	test_grade = correct_answers / correctness.size();
+	return test_grade;
+}*/
+
+void Test::omitQuestion(vector<string> student_answers, int deletedIndex) {
 	student_answers.erase(studentAnswers.at(deletedIndex-1);
-
+}
 #endif
