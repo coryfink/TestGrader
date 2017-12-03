42f9da0f833d6745cab688e8f8743e2020f86cb2
mprovosty <34016170+mprovosty@users.noreply.github.com>
2017-12-03 15:12:12 -0500

    Update Assignment.h
    debugged

--- Assignment.h
+++ Assignment.h
@@ -25,6 +25,7 @@ class Assignment {
 		float mean;
 		float curve;
 		float mode;
+		float median;
 
 	public://Pass in vector containing grades to calculate stats
 		Assignment(string temp_assignment_name);
@@ -32,20 +33,19 @@ class Assignment {
 		void add_test(Test temp);
 		void calculateHigh();
  		void calculateLow();
- 		void calculateMean();
+ 		float calculateMean();
  		void calculateMode();
  		void calculateMedian();
 		void organizeGrades();
 	 	void calculateCurve();
 		void printStatistics();
-  		void displayAll();
+		void displayAll();
 		void omitQuestion(int deletedIndex);
+  
 		//Accessor methods
 		float getHigh();
 		float getLow();
 		float getMean();
-		vector* getGrades();
-		vector* getTest();
 
 		//mutator methods
 		void set_vector_index(float numerical_grade);
@@ -58,7 +58,7 @@ Assignment::Assignment(string temp_assignment_name) {
 }
 
 void Assignment::set_vector_index(float numerical_grade) {
-	grades.push_back(numerical grade);
+	grades.push_back(numerical_grade);
 }
 
 void Assignment::set_name(string name) {
@@ -94,23 +94,25 @@ void Assignment::calculateLow() {
 }
 
 //calculates te average
-void Assignment::calculateMean() {
+float Assignment::calculateMean() {
 	float sum = 0;
 	int size = grades.size();
 	for (int i = 0; i < size; i++) {
 		sum += grades[i];
 	}
 	mean = sum / size;
+	return mean;
 }
 
 //adds a curve to the tests
 void Assignment::calculateCurve() {
+//not finished yet, trying to figure out how to return the "curved" grades vector
 	float inputAverage;
 
 	cout << "Please enter the desired average for this exam." << endl;
 	cin >> inputAverage;
 	
-	float realAverage = calculateMean(grades); //**calculateMean(grades)
+	float realAverage = calculateMean(); //**calculateMean(grades)
 	
 	if (realAverage < inputAverage) {
 		curve = inputAverage - realAverage;
@@ -123,71 +125,67 @@ void Assignment::calculateCurve() {
 	for (int i = 0; i < size; i++) {
 		grades[i] = grades[i] + curve;
 	}
-	cout<<"Please utilize the "Calculate Statistics" option in the user interface to see new assignment statistics accounting for the curve.\n";
+	cout<<"Please utilize the Calculate Statistics option in the user interface to see new assignment statistics accounting for the curve.\n";
 }
 
 //Method to organize the vector that holds all the grades
-void Assignment::organizeGrades(vector<float> grades){
+void Assignment::organizeGrades(){
 	sort(grades.begin(), grades.end());//using sort function for vectors (algorithm header)
 }
 
-//Method to calculate the Median of the grades
-float Assignment::calculateMedian(vector<float> grades) {
-	//Organizing the scores
-	organizeGrades(grades);
-	
-	//Odd student size case: returning the middle grade in the vector
-	if (grades.size()%2 == 1) {
-		return grades[(1+grades.size())/2-1];
-	}
-	//Even student size case: returning the average value between the two middle grades
- 	else {
-		return (grades[grades.size()/2-1] + grades[grades.size()/2])/2;
+//I think .size will give us a bit value of the actual size of the array, not
+//the amt of elements, we may need to use *sizeof(grades)/sizeof(grades[0])*
+//maybe .capacity() is what we are looking for? need to test -mike
+void Assignment::calculateMedian() {
+	//Odd student size case
+	int size = grades.size();
+	if (size % 2 == 1) {
+		median = grades[( 1 + size) / 2 - 1];
+	} else {
+		median = (grades[size / 2 - 1] + grades[size / 2 ]) / 2;
 	}
 }
 
-//Method to calculate the Mode of the grades
-float Assignment::calculateMode(vector<float> grades) {
-	//Organizing the scores and setting the initial mode to 1
-	organizeGrades(grades);
-	int overallMode = 1;
-	int currentMode = 1;
+//assuming grade array will be in order for mode calculation
+void Assignment::calculateMode() {
+	int mode = 1;
+	int currentMode = 0;
 	float possibleMode = grades[0];
-	
-	//Looping through the grades and counting recurring grades
-	for (int i = 1; i < grades.size(); ++i) {
-		if (grades[i] == grades[i-1]){
+	int size = grades.size();	
+	for (int i = 1; i < size; i++) {//loops through grades and counts recurring grades
+		if (grades[i] == grades[i - 1]){
 			currentMode++;
-		}
-		//Setting the possible mode once the current mode surpasses the overall mode
- 		else if (currentMode > overallMode) {
-			possibleMode = grades[i-1];
-			overallMode = currentMode;
-			currentMode = 1;
+		} else if (currentMode > mode) {//sets the possible mode once it is greater than the current
+			possibleMode = grades[i - 1];
+			mode = currentMode;
+			currentMode = 0;
 		}
 	}
-	return possibleMode;
+	mode = possibleMode;
 }
 
 //printing method that displays the grade statistics 
 void Assignment::printStatistics(){
-    cout<<"| Mean: "<< getMean() <<" | High: "<< getHigh() << " | Low: "<< getLow() <<endl;
+    cout<<"| Mean: "<< mean <<" | High: "<< high << " | Low: "<< low <<endl;
 }
 
-//printing method that displays all student names and scores
-void Assignment::displayAll(){
-    vector <string> names = getTest().getName();
-    for(it = getGrades.begin(); itt = names.begin();  it != getGrades.end(); itt = names.end(); it++,itt++ ){
-        cout<< "\t" << *itt << "\t|\t" << *it << endl;
-    }
+void Assignment::displayAll() {
+	vector <string> names = getTest().getName();
+	for(it = getGrades.begin(); itt = names.begin();  it != getGrades.end(); itt = names.end(); it++,itt++ ) {
+		cout<< "\t" << *itt << "\t|\t" << *it << endl;
+	}
 }
+
 void Assignment::omitQuestion(int deletedIndex) {
-	for(int i = 0; i<classTests.size(); i++){
-		if(classTests[i] != NULL){
-			(classTests[i]->student_answers).erase(deletedIndex - 1);
+	for (vector<Test> i = classTests.begin(); i != classTests.end(); i++) {
+		if (classTests[i] != NULL) {
+			(classTests[i]->student_answers).erase(deletedIndex -1);
+		} else {
+			cout << "\n\tThe index is not valid.\n";
 		}
 	}
 }
+
 //accessor methods for grade statistics
 float Assignment::getHigh() {
 	return high;
@@ -200,12 +198,4 @@ float Assignment::getLow() {
 float Assignment::getMean() {
 	return mean;
 }
-vector* Assignment::getGrades() {
-	return *grades;
-}
-
-vector* Assignment::getTest() {
-	return *classTests;
-}
-
 #endif
